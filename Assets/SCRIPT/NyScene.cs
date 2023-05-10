d57710690a9cde9d2528691a677366648581d5d9
MolleVK <gayiverse@gmail.com>
2023-05-10 15:15:05 +0200

    Main Menu

---
+++ Assets/SCRIPT/NyScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// SceneManagement behövdes för att kunna använda sig av flera Scenes i form av "SceneManager" Class och "LoadScene" function och kommer användas
+/// </summary>
+
+public class NyScene : MonoBehaviour
+{
+    public void BytScene(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
